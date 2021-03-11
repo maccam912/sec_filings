d951ff7c6190af0a6070a16f325900474d1a932f
maccam912 <maccam912@gmail.com>
2021-03-10 21:06:10 -0600

    try postgres

--- lib/sec_filings/repo.ex
+++ lib/sec_filings/repo.ex
@@ -1,5 +1,6 @@
 defmodule SecFilings.Repo do
   use Ecto.Repo,
     otp_app: :sec_filings,
-    adapter: Ecto.Adapters.MyXQL
+    # adapter: Ecto.Adapters.MyXQL
+    adapter: Ecto.Adapters.Postgres
 end
