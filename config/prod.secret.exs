8b1150c7faad5b9b4386f84efae4f303da1b2f8b
maccam912 <maccam912@gmail.com>
2021-03-15 14:56:02 -0500

    one connection

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -14,7 +14,7 @@ database_url =
 config :sec_filings, SecFilings.Repo,
   ssl: true,
   url: database_url,
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "100")
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "1")
 
 secret_key_base =
   System.get_env("SECRET_KEY_BASE") ||
