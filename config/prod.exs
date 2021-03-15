279c9c5a5a345c6691fff61c292ea6b12181083a
maccam912 <maccam912@gmail.com>
2021-03-15 14:17:00 -0500

    updated dockerfile, prod hostname

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :sec_filings, SecFilingsWeb.Endpoint,
-  url: [host: "secfilings.koski.co", port: 80],
+  url: [host: "secfilings.dokku.koski.co", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json",
   server: true
 
