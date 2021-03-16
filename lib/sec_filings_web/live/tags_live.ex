865e106058b7e8e7c4fd9e61f2322a120b2de96a
maccam912 <maccam912@gmail.com>
2021-03-16 16:08:49 -0500

    message about cached vs not cached

--- lib/sec_filings_web/live/tags_live.ex
+++ lib/sec_filings_web/live/tags_live.ex
@@ -11,11 +11,41 @@ defmodule SecFilingsWeb.TagsLive do
     adsh = Map.get(params, "adsh")
     cik = Map.get(params, "cik")
 
+    send(self(), :update)
+
     {:ok,
      assign(socket,
        adsh: adsh,
        cik: cik,
-       documents: SecFilings.NumberExtractor.get_documents(cik, adsh)
+       documents: []
      )}
+  end
+
+  @impl true
+  def handle_info(:update, socket) do
+    cik = socket.assigns.cik
+    adsh = socket.assigns.adsh
+    documents = Cachex.get!(:filings_cache, {cik, adsh})
+
+    socket =
+      if is_nil(documents) do
+        put_flash(socket, :info, "Form not cached. Give us 60 seconds...")
+      else
+        socket
+      end
+
+    send(self(), :get_documents)
+
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_info(:get_documents, socket) do
+    cik = socket.assigns.cik
+    adsh = socket.assigns.adsh
+    documents = SecFilings.NumberExtractor.get_documents(cik, adsh)
+
+    socket = clear_flash(socket)
+    {:noreply, assign(socket, documents: documents)}
   end
 end
