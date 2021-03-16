24871d4e9faf713691585044edc5684e832b803a
maccam912 <maccam912@gmail.com>
2021-03-16 15:50:44 -0500

    new table view

--- lib/sec_filings/number_extractor.ex
+++ lib/sec_filings/number_extractor.ex
@@ -1,10 +1,4 @@
 defmodule SecFilings.NumberExtractor do
-  def get_doc(url) do
-    {:ok, %HTTPoison.Response{status_code: 200, body: body}} = HTTPoison.get(url)
-    true = Cachex.put!(:filings_cache, url, body)
-    body
-  end
-
   def parse_desc({"description", [], [desc_text, text]}) do
     {"text", [], doc} = text
 
@@ -28,97 +22,39 @@ defmodule SecFilings.NumberExtractor do
     type = String.trim(type)
     {seq_num, ""} = Integer.parse(String.trim(seq_num))
     filename = String.trim(filename)
+    reports = Floki.find(doc, "table.report")
 
     %{
       type: type,
       sequence_number: seq_num,
       filename: filename,
       description: description,
-      text: doc
+      reports: reports
     }
   end
 
-  def get_documents(body) do
+  def gen_filename(cik, adsh) do
+    "edgar/data/#{cik}/#{adsh}.txt"
+  end
+
+  def gen_url(cik, adsh) do
+    "https://www.sec.gov/Archives/#{gen_filename(cik, adsh)}"
+  end
+
+  def download_documents(cik, adsh) do
+    url = gen_url(cik, adsh)
+    {:ok, %HTTPoison.Response{status_code: 200, body: body}} = HTTPoison.get(url)
     doc = Floki.parse_document!(body)
 
-    Floki.find(doc, "document")
-    |> Enum.map(fn doc -> parse_single_document(doc) end)
+    docs =
+      Floki.find(doc, "document")
+      |> Enum.map(fn doc -> parse_single_document(doc) end)
+
+    {:ok, true} = Cachex.put(:filings_cache, {cik, adsh}, docs)
+    docs
   end
 
-  def get_tag_docs(filename) do
-    url = "https://www.sec.gov/Archives/#{filename}"
-    body = Cachex.get!(:filings_cache, url) || get_doc(url)
-    numbers = Regex.scan(~r/<ix:nonFraction[^>]*>[^<]*<\/ix:nonFraction>/, body)
-    numbers
-  end
-
-  def extract_tags(tag_docs) do
-    tag_docs
-    |> Enum.map(fn item ->
-      case Floki.parse_document!(item) do
-        [{"ix:nonfraction", attrs, [num]}] ->
-          fixed_num =
-            num
-            |> String.replace(",", "")
-            |> String.replace("$", "")
-
-          parsed_num = Float.parse(fixed_num)
-
-          new_attrs =
-            Enum.reduce(attrs, %{}, fn {name, value}, acc ->
-              Map.put(acc, name, value)
-            end)
-
-          {new_attrs, parsed_num}
-
-        [{_, _, []}] ->
-          nil
-      end
-    end)
-    |> Enum.filter(fn item -> not is_nil(item) end)
-    |> Enum.filter(fn {_, parsed_num} ->
-      parsed_num != :error
-    end)
-    |> Enum.map(fn {attrs, {num, _}} ->
-      if not is_nil(Map.get(attrs, "scale")) do
-        case Integer.parse(Map.get(attrs, "scale")) do
-          {dec_movement, ""} ->
-            scaled_num = num * :math.pow(10, dec_movement)
-            Map.put(attrs, "value", num)
-            Map.put(attrs, "fixed_value", scaled_num)
-
-          _ ->
-            %{}
-        end
-      else
-        %{}
-      end
-    end)
-    |> Enum.reduce(%{}, fn item, acc ->
-      key = Map.get(item, "name")
-
-      if not Map.has_key?(acc, key) do
-        Map.put(acc, key, item)
-      else
-        acc
-      end
-    end)
-  end
-
-  def fixed_value_gaap_tags(tags) do
-    tags
-    |> Enum.reduce(%{}, fn {k, v}, acc ->
-      case v do
-        %{"fixed_value" => _} -> Map.put(acc, k, v)
-        _ -> acc
-      end
-    end)
-    |> Enum.reduce(%{}, fn {k, v}, acc ->
-      if String.contains?(k, "us-gaap") do
-        Map.put(acc, k, v)
-      else
-        acc
-      end
-    end)
+  def get_documents(cik, adsh) do
+    Cachex.get!(:filings_cache, {cik, adsh}) || download_documents(cik, adsh)
   end
 end
--- lib/sec_filings_web/live/tags_live.ex
+++ lib/sec_filings_web/live/tags_live.ex
@@ -6,25 +6,16 @@ defmodule SecFilingsWeb.TagsLive do
     "edgar/data/#{cik}/#{adsh}.txt"
   end
 
-  def get_tags(cik, adsh) do
-    tag_docs = SecFilings.NumberExtractor.get_tag_docs(gen_filename(cik, adsh))
-    extracted = SecFilings.NumberExtractor.extract_tags(tag_docs)
-    SecFilings.NumberExtractor.fixed_value_gaap_tags(extracted)
-  end
-
   @impl true
   def mount(params, _session, socket) do
     adsh = Map.get(params, "adsh")
     cik = Map.get(params, "cik")
-    tags = get_tags(cik, adsh)
 
-    ordered_tag_keys =
-      tags
-      |> Enum.sort_by(fn {_, v} ->
-        -1 * Map.get(v, "fixed_value")
-      end)
-      |> Enum.map(fn {k, _} -> k end)
-
-    {:ok, assign(socket, tags: tags, tag_keys: ordered_tag_keys, adsh: adsh, cik: cik)}
+    {:ok,
+     assign(socket,
+       adsh: adsh,
+       cik: cik,
+       documents: SecFilings.NumberExtractor.get_documents(cik, adsh)
+     )}
   end
 end
