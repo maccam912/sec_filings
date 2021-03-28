30819ab0ff66ef7ac61cdf5b21cea0230faed3dc
maccam912 <maccam912@gmail.com>
2021-03-27 22:01:40 -0500

    trying to get tags

--- lib/sec_filings_web/live/cik_live.ex
+++ lib/sec_filings_web/live/cik_live.ex
@@ -20,6 +20,27 @@ defmodule SecFilingsWeb.CikLive do
     List.first(String.split(adsh_txt, ["."]))
   end
 
+  def get_tags(cik) do
+    filings_q =
+      from i in SecFilings.Raw.Index,
+        join: parsed_documents in assoc(i, :parsed_documents),
+        where:
+          i.form_type in ["10-K", "10-Q"] and i.cik == ^cik and
+            not is_nil(parsed_documents)
+
+    contexts_q =
+      from c in SecFilings.Context,
+        join: i in subquery(filings_q),
+        where: i.id == c.index_id
+
+    tags_q =
+      from t in SecFilings.Tag,
+        join: c in subquery(contexts_q),
+        where: t.context_id == t.id and t.tag in ^Map.keys(@tags)
+
+    SecFilings.Repo.all(tags_q)
+  end
+
   @impl true
   def mount(params, _session, socket) do
     filings =
@@ -32,13 +53,15 @@ defmodule SecFilingsWeb.CikLive do
           order_by: [desc: :date_filed, asc: :form_type]
       )
 
+    tags = IO.inspect(get_tags(Map.get(params, "cik")))
+
     socket =
       assign(socket,
         params: params,
         cik: Map.get(params, "cik"),
         tables: filings,
         debug: "",
-        data: %{}
+        data: tags
       )
 
     {:ok, socket}
