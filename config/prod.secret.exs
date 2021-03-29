5d3a00c635f4e1f627dc29ef58a431eb570a317e
maccam912 <maccam912@gmail.com>
2021-03-28 20:01:04 -0500

    longer timeout

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -15,6 +15,7 @@ config :sec_filings, SecFilings.Repo,
   ssl: true,
   url: database_url,
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+  timeout: 60000
 
 secret_key_base =
   System.get_env("SECRET_KEY_BASE") ||
