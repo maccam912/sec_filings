c13d8421a2215540ac99e60208edcff68e73b051
maccam912 <maccam912@gmail.com>
2021-03-29 10:30:06 -0500

    batches of 400?

--- lib/sec_filings/parser_worker.ex
+++ lib/sec_filings/parser_worker.ex
@@ -105,7 +105,7 @@ defmodule SecFilings.ParserWorker do
 
   def process_batch(docs) do
     docs
-    |> Flow.from_enumerable(stages: 40, min_demand: 0, max_demand: 40)
+    |> Flow.from_enumerable(stages: 40, min_demand: 40, max_demand: 80)
     |> Flow.map(fn index ->
       [_, _, cik, adsh, _] = String.split(index.filename, ["/", "."])
 
@@ -136,7 +136,7 @@ defmodule SecFilings.ParserWorker do
 
   @impl true
   def handle_info(:update, []) do
-    process_n(40)
+    process_n(400)
     Process.send_after(__MODULE__, :update, 1000 * 3)
     {:noreply, []}
   end
