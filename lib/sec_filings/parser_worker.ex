969e1c91a87da07a7a60f08c95b0229ca993873b
maccam912 <maccam912@gmail.com>
2021-03-29 10:21:10 -0500

    put flow back in

--- lib/sec_filings/parser_worker.ex
+++ lib/sec_filings/parser_worker.ex
@@ -105,17 +105,15 @@ defmodule SecFilings.ParserWorker do
 
   def process_batch(docs) do
     docs
-    # |> Flow.from_enumerable(stages: 10, min_demand: 5, max_demand: 10)
-    # |> Flow.map(fn index ->
-    |> Enum.map(fn index ->
+    |> Flow.from_enumerable(stages: 10, min_demand: 5, max_demand: 10)
+    |> Flow.map(fn index ->
       [_, _, cik, adsh, _] = String.split(index.filename, ["/", "."])
 
       SecFilings.Util.generate_url(cik, adsh)
       |> SecFilings.DocumentGetter.get_doc()
       |> process_document(cik, adsh)
     end)
-
-    # |> Flow.run()
+    |> Flow.run()
   end
 
   def process_all() do
